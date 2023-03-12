1093fa310ecca54bba8449adeb57a637834ae970
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-03-12 13:00:21 -0300

    altera o local das strings

--- script.js
+++ script.js
@@ -1,51 +1,9 @@
-const botaoDetalhes = document.getElementsByClassName("botaoDetalhes")
+import bc from "./data/data.json" assert { type: "json" };
 
-
+const botaoDetalhes = document.getElementsByClassName("botaoDetalhes");
 
 for (let i = 0; i < botaoDetalhes.length; i++) {
-    botaoDetalhes[i].addEventListener("click",()=>{
-        if (i===0) {
-            alert("rede na praia pós pedido :)")
-        }
-        if (i===1) {
-            alert("primeira vez que nos encontramos(clássica)")
-        }
-        if (i===2) {
-            alert("eu e você no cumbuco dia 31/12 antes de ir à praia")
-        }
-        
-        if (i===3) {
-            alert("tenho quase certeza que foi o dia da pizzaria nova")
-        }
-        
-        if (i===4) {
-            alert("eu e você no cinema (aquele filme da escritora emily)")
-        }
-        
-        if (i===5) {
-            alert("eu e você no jeronimo depois do cumbuco")
-        }
-        
-        if (i===6) {
-            alert("era domingo e estávamos de saída do cumbuco")
-        }
-        
-        if (i===7) {
-            alert("nossa última ida ao cumbuco (por enquanto)")    
-        }
-        if (i===8) {
-            alert("nós no aniversário(sua perna estava queimada)")    
-        }
-        if (i===9) {
-            alert("academia pré carnaval")    
-        }
-        if (i===10) {
-            alert("preparando pra ir ao aniversário ")    
-        }
-        if (i===11) {
-            alert("foto tirada no aniversáiro que estávamos nos preparando")    
-        }
-        
-    })
-    
+	botaoDetalhes[i].addEventListener("click", () => {
+        alert(bc.data[i])
+    });
 }
