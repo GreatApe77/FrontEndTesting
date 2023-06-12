3ba19da814487d966161d3ca5b5f0f941ef3ec26
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2023-06-12 00:06:33 -0300

    quase funcionando completamente

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
 
+import  DATA  from "./data/data.json" assert {type: "json"};
 
+const descricoes = DATA.descricoes
 
-function renderContainer(index){
-    const albumContainer = document.createElement("div")
-    
+function renderContainer(index, textContent) {
+
+    //setar a div maior
+
+    const fotoContainer = document.createElement("div")
+    fotoContainer.className = "fotoContainer"
+    //setar a imagem
     const image = document.createElement("img")
     image.className = "imagem"
+    image.src = `./img/${index+1}.jpg`
+
+    //setar o paragrafo
+
+    const paragrafo = document.createElement("p")
+    paragrafo.className = "descricao"
 
 
+    paragrafo.textContent = textContent[index] || ""
+    paragrafo.style.display = "none"
+    //renderizar o botao
+
+    const mostrarDetalhesBtn = document.createElement("button")
+    let ativado = false
+    mostrarDetalhesBtn.textContent = "Mostrar Detalhes"
+    mostrarDetalhesBtn.id = "mostrarDetalhesBtn"
+    mostrarDetalhesBtn.className = "botao"
+    mostrarDetalhesBtn.addEventListener("click", () => {
+
+        if (ativado) {
+            paragrafo.style.display = "none"
+            ativado = false
+        } else {
+            paragrafo.style.display = "block"
+            ativado = true
+        }
+    })
+    fotoContainer.appendChild(image)
+    fotoContainer.appendChild(mostrarDetalhesBtn)
+    fotoContainer.appendChild(paragrafo)
+    return fotoContainer
 }
 
 
+for (let i = 0; i < 10; i++) {
+
+    document.getElementById("mainContainer").appendChild(renderContainer(i, descricoes))
+
+}
