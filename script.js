aa3a5076f25209ea4c3bf7cd48427b005387b0b2
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2023-06-11 22:37:45 -0300

    header pronto

--- script.js
+++
@@ -1,9 +0,0 @@
-import bc from "./data/data.json" assert { type: "json" };
-
-const botaoDetalhes = document.getElementsByClassName("botaoDetalhes");
-
-for (let i = 0; i < botaoDetalhes.length; i++) {
-	botaoDetalhes[i].addEventListener("click", () => {
-        alert(bc.data[i])
-    });
-}
