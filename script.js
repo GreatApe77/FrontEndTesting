1126b893c7f6606394dcd0607f3a7597298c935a
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2023-06-11 23:20:49 -0300

    progresso

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+function renderContainer(index){
+    const albumContainer = document.createElement("div")
+    const image = document.createElement("img")
+    image.className = "imagem"
+
+
+}
+
+
